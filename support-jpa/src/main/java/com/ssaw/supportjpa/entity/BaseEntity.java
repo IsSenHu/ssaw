1563e5b3024554b08fe19f501e32f60920b38a99
husen233 <sen.hu@biz-united.com.cn>
2018-12-12 11:13:53 +0800

    ok

---
+++ support-jpa/src/main/java/com/ssaw/supportjpa/entity/BaseEntity.java
@@ -0,0 +1,69 @@
+package com.ssaw.supportjpa.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author HuSen.
+ * @date 2018/12/12 10:33.
+ */
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "create_time")
+    private LocalDateTime createTime;
+
+    @Column(name = "modify_time")
+    private LocalDateTime modifyTime;
+
+    @Column(name = "create_man")
+    private String createMan;
+
+    @Column(name = "modify_man")
+    private String modifyMan;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(LocalDateTime modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public String getCreateMan() {
+        return createMan;
+    }
+
+    public void setCreateMan(String createMan) {
+        this.createMan = createMan;
+    }
+
+    public String getModifyMan() {
+        return modifyMan;
+    }
+
+    public void setModifyMan(String modifyMan) {
+        this.modifyMan = modifyMan;
+    }
+}
